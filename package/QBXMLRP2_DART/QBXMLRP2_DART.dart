56c252aa0010951bb1713e52ffa72b7c491bcb80
Thomas Caserta <tomcaserta@gmail.com>
2014-01-17 09:31:57 +0000

    Added pubspec and move files into relevant place

---
+++ package/QBXMLRP2_DART/QBXMLRP2_DART.dart
@@ -0,0 +1,8 @@
+library QBXMLRP2_DART;
+
+import 'dart:async';
+import 'dart:isolate';
+import 'dart-ext:dll/QBXMLRP2_DART';
+
+part 'lib/connector.dart';
+part 'lib/qb_file_mode.dart';
--- package/QBXMLRP2_DART/lib/connector.dart
+++ package/QBXMLRP2_DART/lib/connector.dart
@@ -1,16 +1,5 @@
-library QuickbooksConnector;
+part of QBXMLRP2_DART;
 
-import 'dart:async';
-import 'dart:isolate';
-import 'dart-ext:QBXMLRP2_DART';
-
-class QBFileMode {
-  static QBFileMode doNotCare = new QBFileMode._create(2);
-  static QBFileMode multiUser = new QBFileMode._create(1);
-  static QBFileMode singleUser = new QBFileMode._create(0);
-  int _n;
-  QBFileMode._create(int this._n);
-}
 class QuickbooksConnector {
   static SendPort _port;
     
